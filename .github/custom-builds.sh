cfbbf5a80ede6263efc3982e9daec8282f6d0c4c
Veerendra <vk.tyk23@simplelogin.com>
2024-11-03 23:12:35 +0100

    Fix Multiarch in Custom Scrip (#41)
    * Fix Multiarch in Custom Scrip
    
    * Fix script
    
    * remove test job in ci.yml
    
    * remove test job in ci.yml
    
    ---------
    
    Co-authored-by: veerendra <veerendra2@github.com>

--- .github/custom-builds.sh
+++ .github/custom-builds.sh
@@ -3,20 +3,20 @@
 set -e
 
 # ----------------------- it-tools with subpath `/it-tools/` -----------------------
-LATEST_TAG=$(curl -sS  https://api.github.com/repos/CorentinTh/it-tools/releases/latest | jq -r '.tag_name')
+LATEST_TAG=$(curl -sS https://api.github.com/repos/CorentinTh/it-tools/releases/latest | jq -r '.tag_name')
 curl -sSOL "https://github.com/CorentinTh/it-tools/archive/refs/tags/${LATEST_TAG}.tar.gz"
 DIR_NAME=$(tar -tf ${LATEST_TAG}.tar.gz | head -1)
 tar -xf ${LATEST_TAG}.tar.gz
 pushd ${DIR_NAME} 1>/dev/null
 sed -i '/RUN pnpm build/i ENV BASE_URL="/it-tools/"' Dockerfile
-docker build --platform linux/arm64/v8,linux/amd64 -t veerendra2/it-tools:latest .
-docker push veerendra2/it-tools:latest
+docker buildx build --platform linux/arm64/v8,linux/amd64 -t veerendra2/it-tools:latest . --push
+popd 1>/dev/null
 
 # ----------------------- pwgen multi arch docker image -----------------------
-LATEST_TAG=$(curl -sS  https://api.github.com/repos/jocxfin/pwgen/releases/latest | jq -r '.tag_name')
+LATEST_TAG=$(curl -sS https://api.github.com/repos/jocxfin/pwgen/releases/latest | jq -r '.tag_name')
 curl -sSOL "https://github.com/jocxfin/pwgen/archive/refs/tags/${LATEST_TAG}.tar.gz"
-DIR_NAME=$(tar -tf ${LATEST_TAG}.tar.gz | head -1
+DIR_NAME=$(tar -tf ${LATEST_TAG}.tar.gz | head -1)
 tar -xf ${LATEST_TAG}.tar.gz
 pushd ${DIR_NAME} 1>/dev/null
-docker build --platform linux/arm64/v8,linux/amd64 -t veerendra2/pwgen:latest .
-docker push veerendra2/pwgen:latest
+docker buildx build --platform linux/arm64/v8,linux/amd64 -t veerendra2/pwgen:latest . --push
+popd 1>/dev/null
